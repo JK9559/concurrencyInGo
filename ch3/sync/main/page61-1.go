4e610523a0ddf6599bdca424b650937135d0da71
jk9559 <2272728555@qq.com>
2019-07-09 00:51:00 +0800

    互斥锁

--- ch3/sync/main/page61-1.go
+++ ch3/sync/main/page61-1.go
@@ -1,14 +1,52 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
+
+/**
+sync.Mutex 互斥锁
+*/
 
 func main() {
 	var count int
+	// 互斥锁
 	var lock sync.Mutex
 
 	increment := func() {
+		// 我们请求对临界区的独占 使用互斥锁来解决
+		lock.Lock()
+		// 声明已经完成了对临界区的独占使用 释放互斥锁
+		defer lock.Unlock()
+		count++
+		fmt.Printf("Incrementing:%d\n", count)
+	}
+
+	decrement := func() {
 		lock.Lock()
 		defer lock.Unlock()
-
+		count--
+		fmt.Printf("Decrementing:%d\n", count)
 	}
+
+	var arithmetic sync.WaitGroup
+	for i := 0; i <= 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			increment()
+		}()
+	}
+
+	for i := 0; i <= 5; i++ {
+		arithmetic.Add(1)
+		go func() {
+			defer arithmetic.Done()
+			decrement()
+		}()
+	}
+
+	arithmetic.Wait()
+	fmt.Println("Arithmetic complete")
 }
