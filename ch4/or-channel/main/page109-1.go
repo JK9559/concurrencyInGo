0b4d47c955e2858e7aa32c7ba4fa87d8eef95eec
wkang <2272728555@qq.com>
2019-08-29 12:40:45 +0800

    select

--- ch4/or-channel/main/page109-1.go
+++ ch4/or-channel/main/page109-1.go
@@ -9,7 +9,12 @@ import (
 	"time"
 )
 
+/*
+Go or-channel模式 为多个协程并发 如果有一个协程结束 那么这一组协程都将结束
+*/
+
 func main() {
+	// 声明一个函数 接收参数为 <-chan的切片，返回参数为<-chan
 	var or func(channels ...<-chan interface{}) <-chan interface{}
 
 	or = func(channels ...<-chan interface{}) <-chan interface{} {
@@ -42,8 +47,6 @@ func main() {
 					fmt.Println("this is chan 1")
 				case <-channels[2]:
 					fmt.Println("this is chan 2")
-				// 执行了or 但是执行结果为阻塞 因为orDone未被读取被阻塞 之后close后 才释放，所以是先 channels[1]后 执行完毕
-				// 这个go func 最后释放orDone
 				case <-or(append(channels[3:], orDone)...):
 					fmt.Println("this is chan 3")
 				}
@@ -56,7 +59,7 @@ func main() {
 		c := make(chan interface{})
 		go func() {
 			//fmt.Println("this func is:", after)
-			//defer fmt.Println("this is:", after)
+			defer fmt.Println("this is:", after)
 			defer close(c)
 			time.Sleep(after)
 		}()
@@ -68,10 +71,19 @@ func main() {
 	<-or(
 		sig(2*time.Hour),
 		//sig(1*time.Minute),
-		//sig(1*time.Hour),
-		sig(1*time.Second),
+		sig(1*time.Hour),
 		sig(2*time.Minute),
+		sig(1*time.Second),
 	)
 
+	// 执行流程是
+	// 1. 传入3个只读channel 长度为3 创建orDone 进入go func的default分支。
+	// 2. 分别执行default内的4个case,前三个case由于睡眠时间未到，全为阻塞状态
+	// 3. 第四个分支调递归调用or函数，此时入参只有orDone 阻塞，所以在等待1秒钟之后执行完毕释放orDone
+
+	// 当传入为4个channel的时候
+	// 1. 长度为4 创建orDone 进入go func的default分支。
+	// 2. 分别执行default内的4个case,前三个case由于睡眠时间未到，全部阻塞
+	// 3. 第四个分支递归调用or函数，入参为1second chan和orDone，读取1second后 递归结束
 	fmt.Printf("done after %v", time.Since(start))
 }
