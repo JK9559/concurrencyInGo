7538d9283cc1885cb17c09a2c742626f780ad891
wkang <2272728555@qq.com>
2019-10-08 11:14:37 +0800

    go in DS&A

--- GoinDS&A/Dynamic Planning-动态规划/main/连续子数组最大和.go
+++ GoinDS&A/Dynamic Planning-动态规划/main/连续子数组最大和.go
@@ -14,24 +14,24 @@ func max(i, j int) int {
 	}
 }
 
-func longestSubArrayMaxSum(arr []int) int {
-	if len(arr) == 1 {
-		return arr[0]
+func longestSubArrayMaxSum(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
 	}
-	if len(arr) == 0 {
+	if len(nums) == 0 {
 		return 0
 	}
-	var resLen, resj, res = 1, 0, arr[0]
+	var resLen, resj, res = 1, 0, nums[0]
 
-	var arrLen = len(arr)
+	var arrLen = len(nums)
 	var dp []int = make([]int, arrLen)
 
-	dp[0] = arr[0]
+	dp[0] = nums[0]
 	for i := 1; i < arrLen; i++ {
-		dp[i] = max(arr[i], dp[i-1]+arr[i])
+		dp[i] = max(nums[i], dp[i-1]+nums[i])
 		if dp[i] > res {
 			resLen++
-			if arr[i] == dp[i] {
+			if nums[i] == dp[i] {
 				resLen = 1
 			}
 			resj = i
@@ -41,7 +41,7 @@ func longestSubArrayMaxSum(arr []int) int {
 	}
 
 	fmt.Println(dp)
-	fmt.Println(resj-resLen+1, resj, arr[resj-resLen+1:resj+1])
+	fmt.Println(resj-resLen+1, resj, nums[resj-resLen+1:resj+1])
 	return res
 
 }
